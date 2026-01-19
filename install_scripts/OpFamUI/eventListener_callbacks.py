3650cdb82fa5721798ff50685d56ebad25a80aed
Function Store <47873029+function-store@users.noreply.github.com>
2026-01-19 05:47:38 +0100

    #33 Improve family install/uninstall event handling to UI
    Introduced onRegistryFamilyInstalled and onRegistryFamilyUninstalled methods in OpFamUIExt to handle family registry changes. Updated eventListener_callbacks to call these methods on family install/uninstall events, ensuring the UI responds appropriately to changes in the family registry.

--- install_scripts/OpFamUI/eventListener_callbacks.py
+++ install_scripts/OpFamUI/eventListener_callbacks.py
@@ -7,9 +7,11 @@ def onFamilyUnregistered( family_name:str ):
 	return
 
 def onFamilyInstalled( family_name:str, family_owner:OP ):
+	ext.OpFamUIExt.onRegistryFamilyInstalled(family_name, family_owner)
 	return
 
 def onFamilyUninstalled( family_name:str ):
+	ext.OpFamUIExt.onRegistryFamilyUninstalled(family_name)
 	return
 
 def onFamilyRenamed( old_name:str, new_name:str, family_owner:OP ):
