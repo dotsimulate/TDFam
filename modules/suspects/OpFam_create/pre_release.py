2bc30e780bc03224337b67dd1fb6011af28ab38d
Function Store <47873029+function-store@users.noreply.github.com>
2026-04-21 01:40:18 +0200

    Update pre_release.py

--- modules/suspects/OpFam_create/pre_release.py
+++ modules/suspects/OpFam_create/pre_release.py
@@ -1,10 +1,17 @@
 '''Info Header Start
 Name : pre_release
 Author : Dan@DAN-4090
-Saveorigin : opfam-create_dev.56.toe
+Saveorigin : opfam-create_dev.64.toe
 Saveversion : 2023.12370
 Info Header End'''
 
 internal = op('internal_pars')
 for _par in internal.customPars:
     _par.reset()
+
+parent().par.Targettype = ''
+parent().par.Targetop = ''
+parent().par.Targetcomp = ''
+parent().par.Opfolder = ''
+parent().par.Opcomp = ''
+parent().par.Compatibletypes = ''
